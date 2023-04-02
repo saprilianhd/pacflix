53dcf96221ffab016dd04a280c92692fee569dbd
unknown <saprilianh@gmail.com>
2023-04-02 23:00:04 +0700

    Menampilkan user_do

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 print("Halo ini didalam dokumen main.py")
 
 user_do = input("Masukkan username Anda :")
-<<<<<<< HEAD
+
 print(user_do)
-=======
 
 if makan = 2:
     print("akan kenyang")
 else:
     print("Lapar")
 
->>>>>>> 3f86ad21d1b5112ecc94032a22ee085c2ef48a5a
